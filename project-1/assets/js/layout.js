075da9931d7e7c79782905f625fa925d84b6bec7
Chinara0303 <chinaraei@code.edu.az>
2024-02-05 17:35:56 +0400

    Feat:Added blog page

--- project-1/assets/js/layout.js
+++ project-1/assets/js/layout.js
@@ -25,15 +25,19 @@ let burgerMenu = document.querySelector('.burger-menu');
 let menu = document.querySelector('.menu-side');
 let openSubmenues = document.querySelectorAll(".open-submenu");
 let overlay = document.querySelector('.overlay');
+let secondLine = document.querySelector('.burger-menu .second-line')
 
 burgerMenu.addEventListener("click", function () {
     menu.classList.toggle("active-menu");
     overlay.style.display = "block";
+    secondLine.classList.toggle("active-line")
 })
 
 overlay.addEventListener("click", function () {
     menu.classList.toggle("active-menu");
     overlay.style.display = "none";
+    secondLine.classList.remove("active-line")
+
 })
 
 for (let i = 0; i < openSubmenues.length; i++) {
@@ -42,3 +46,23 @@ for (let i = 0; i < openSubmenues.length; i++) {
         submenu.classList.toggle("d-none");
     })
 }
+
+
+// scroll to top button
+let scrollToTop = document.getElementById('scroll-to-top')
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 400) {
+        scrollToTop.style.opacity = '1';
+        scrollToTop.style.visibility = 'visible';
+    } else {
+        scrollToTop.style.opacity = '0';
+        scrollToTop.style.visibility = 'hidden';
+    }
+});
+
+scrollToTop.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
