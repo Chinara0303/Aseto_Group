c8187db9266e2b13c7310fcee89d1b7f5df303e9
Chinara0303 <chinaraei@code.edu.az>
2024-02-06 17:36:08 +0400

    Feat:Added products page

---
+++ project-1/assets/js/category.js
@@ -0,0 +1,18 @@
+let listView = document.querySelector('.list-view');
+let gridView = document.querySelector('.grid-view');
+let gridMode = document.querySelector('.grid-mode');
+let listMode = document.querySelector('.list-mode');
+
+listMode.addEventListener('click',function(){
+    listMode.classList.add('active-mode');
+    gridMode.classList.remove('active-mode');
+    listView.classList.remove('de-active-view');
+    gridView.classList.add('de-active-view');
+})
+
+gridMode.addEventListener('click',function(){
+    listMode.classList.remove('active-mode');
+    gridMode.classList.add('active-mode')
+    gridView.classList.remove('de-active-view');
+    listView.classList.add('de-active-view');
+})
